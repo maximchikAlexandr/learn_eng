391110a95a293362c58d9562a535ce375e2ecc8f
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-12 22:51:33 +0300

    Add endpoint 'index'

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
-from config import Configuration
+from config import ConfigurationTest
 
 app = Flask(__name__)
-app.config.from_object(Configuration)
+app.config.from_object(ConfigurationTest)
+
+@app.route('/')
+def index():
+    return 'Main page'
+
 
 if __name__ == '__main__':
     app.run()
