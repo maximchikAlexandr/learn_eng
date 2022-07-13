49d6dfdac7a14f81677b65e3f21a1d4109f8bf71
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-13 22:27:47 +0300

    Add setting of DB

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Configuration(object):
 class ConfigurationTest(object):
     DEBUG = True
     SECRET_KEY = 'eeda3f35d9d4482f6eb1d34c11ccb08781633acf'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dct/dct_eng.db'
+    SQLALCHEMY_BINDS = {'user_bp' : 'sqlite:///user/users.db'}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
