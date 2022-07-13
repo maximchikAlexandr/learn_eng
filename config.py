23b1419f2b6e26dddb11441e280f6cacdf6cb731
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-13 19:18:04 +0300

    Add secret_key

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Configuration(object):
     DEBUG = False
-
+    SECRET_KEY = 'eeda3f35d9d4482f6eb1d34c11ccb08781633acf'
 
 class ConfigurationTest(object):
     DEBUG = True
+    SECRET_KEY = 'eeda3f35d9d4482f6eb1d34c11ccb08781633acf'
