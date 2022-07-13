5349e5b4fbf8bd4e9f949911f524f370102a0043
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-13 18:12:46 +0300

    register BP 'user'

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from config import ConfigurationTest
+from user.main import user_bp
 
 app = Flask(__name__)
 app.config.from_object(ConfigurationTest)
+app.register_blueprint(user_bp, url_prefix='/user')
 
 @app.route('/')
 def index():
