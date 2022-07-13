dda9e1a41181e2ddc3ca3b6040515c25dd8572a2
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-13 22:28:19 +0300

    Create 'repository.py'

---
+++ user/repository.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    __bind_key__ = 'user_bp'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False)
+    hash_psw = db.Column(db.String(255), nullable=False)
+
+class UserDB:
+
+    def __init__(self, db):
+        self.__db = db
+
+    def add_new_user(self, username, email, hashpsw):
+        user = User(username=username,
+                    email=email,
+                    hashpsw=hashpsw)
+
+        self.__db.session.add(user)
+        self.__db.session.commit()
