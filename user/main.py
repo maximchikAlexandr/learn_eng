b3c181bb0d50e16b18fcf53d01aeb5563165b5cb
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-13 18:09:30 +0300

    create main.py. add route '/login'

---
+++ user/main.py
@@ -0,0 +1,10 @@
+from flask import render_template,Blueprint, abort
+
+user_bp = Blueprint('user', __name__, template_folder='templates', static_folder='static')
+
+@user_bp.route('/login')
+def login():
+    try:
+        return render_template('login/html')
+    except:
+        abort(404)
