351784b4d9d136c26c98af7435e079c54ded0963
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-16 18:11:05 +0300

    Refactoring code

--- user/main.py
+++ user/main.py
@@ -18,18 +18,19 @@ def login():
 def register():
     if request.method == 'POST':
         check_dct = check_data_user(request)
+
         # if data in form is correct
         if check_dct['password_ok']:
             hash_psw = generate_password_hash(request.form['psw'])
-
             if userDB.add_new_user(username=request.form['name'],
                          email=request.form['email'],
                          hashpsw=hash_psw):
                 flash(f'User «{request.form["name"]}» is registred in the site',
                       category='success')
                 return render_template('login.html')
+
         # if data in form is not correct
-        if len(check_dct['flashed_messages']) > 0:
+        if check_dct['flashed_messages']:
             for msg in check_dct['flashed_messages']:
                 flash(msg, category='danger')
 
