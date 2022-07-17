8311615b96d1d880116b7155a8f3491805dddcc3
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-17 17:34:05 +0300

    Fix: email is unique value in DB

--- user/models.py
+++ user/models.py
@@ -7,6 +7,6 @@ class User(db.Model, UserMixin):
     __bind_key__ = 'user_bp'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     hash_psw = db.Column(db.String(255), nullable=False)
     time = db.Column(db.Integer(), nullable=False)
