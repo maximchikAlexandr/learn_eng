5a1acbcd97032785a4c3c36a4962874e462a38e5
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-17 17:32:31 +0300

    Change logic of func '_searching_error': False -> True

--- user/service.py
+++ user/service.py
@@ -1,17 +1,17 @@
 def _searching_error(username: str, email: str, psw: str, psw2: str) -> dict:
 
     # calculating the length
-    min_len_name_error = len(username) > 6
-    max_len_name_error = len(username) < 20
-    min_len_psw_error = len(psw) > 6
-    max_len_psw_error = len(psw) < 20
+    min_len_name_error = len(username) < 4
+    max_len_name_error = len(username) > 20
+    min_len_psw_error = len(psw) < 6
+    max_len_psw_error = len(psw) > 20
 
-    equal_psw_error = psw == psw2 # check equal passwords
-    digit_error = len([ch for ch in psw if str(ch) in '0123456789']) > 3 # searching for digits
-    uppercase_error = psw != psw.lower() # searching for uppercase
-    lowercase_error = psw != psw.upper() # searching for lowercase
-    ascii_error = psw.isascii()  #  searching for ascii
-    unique_symbols_error = len(set(psw)) / len(psw) > 0.75 # check count of a unique symbols
+    equal_psw_error = psw != psw2 # check equal passwords
+    digit_error = len([ch for ch in psw if str(ch) in '0123456789']) < 3 # searching for digits
+    uppercase_error = psw == psw.lower() # searching for uppercase
+    lowercase_error = psw == psw.upper() # searching for lowercase
+    ascii_error = not psw.isascii()  #  searching for ascii
+    unique_symbols_error = len(set(psw)) / len(psw) < 0.75 # check count of a unique symbols
 
     return  {
         'max_len_psw_error' : max_len_psw_error,
@@ -37,7 +37,7 @@ def check_data_user(request)  -> dict:
         'max_len_psw_error' : 'Max length of password is 20 symbols',
         'min_len_psw_error': 'Min length of password is 6 symbols',
         'max_len_name_error': 'Max length of username is 20 symbols',
-        'min_len_name_error': 'Min length of username is 6 symbols',
+        'min_len_name_error': 'Min length of username is 4 symbols',
         'digit_error' : 'Password must contain 3 or more digits',
         'equal_psw_error' : 'Passwords is not equal',
         'uppercase_error' : 'Password must contain 1 or more symbols in upercase',
@@ -47,6 +47,6 @@ def check_data_user(request)  -> dict:
     }
 
     return {
-        'password_ok' : all(check_dct.values()),
-        'flashed_messages' : [flashed_messages[key] for key, value in check_dct.items() if not value]
+        'password_ok' : not all(check_dct.values()),
+        'flashed_messages' : [flashed_messages[key] for key, value in check_dct.items() if value]
     }
