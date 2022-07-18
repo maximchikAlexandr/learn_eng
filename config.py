f1e280d02ca3021ab2b32bf4d8775909a6e62d64
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-18 20:01:30 +0300

    Add object ConfigurationBase

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
-class Configuration(object):
-    DEBUG = False
-    SECRET_KEY = 'eeda3f35d9d4482f6eb1d34c11ccb08781633acf'
-
-class ConfigurationTest(object):
-    DEBUG = True
+class ConfigurationBase(object):
     SECRET_KEY = 'eeda3f35d9d4482f6eb1d34c11ccb08781633acf'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dct/data/dct_eng.db'
     SQLALCHEMY_BINDS = {'user_bp' : 'sqlite:///user/data/users.db'}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Configuration(ConfigurationBase):
+    DEBUG = False
+
+
+class ConfigurationTest(ConfigurationBase):
+    DEBUG = True
+
