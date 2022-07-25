c321abb8f6ce4ce8b963ab115ce01e29ad5bb834
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-07-25 20:25:52 +0300

    Refactoring functions '_get_examples' and '_get_synonyms'

--- dct/service.py
+++ dct/service.py
@@ -5,12 +5,13 @@ from  dct.service.config_yandex_dct import yandexDictonaryKey
 def _get_examples(response):
     res_dct = {}
     for pos in response['def']:
-        key, values = pos['pos'], []
-        for item in pos['tr']:
-            if 'ex' in item:
-                for example in item['ex']:
-                    values.append(f"{example['text']} – {example['tr'][0]['text']}")
-            if values: res_dct[key] = values
+        if 'pos' in pos:
+            key, values = pos['pos'], []
+            for item in pos['tr']:
+                if 'ex' in item:
+                    for example in item['ex']:
+                        values.append(f"{example['text']} – {example['tr'][0]['text']}")
+                if values: res_dct[key] = values
 
     return res_dct
 
@@ -18,12 +19,13 @@ def _get_examples(response):
 def _get_synonyms(response):
     res_dct = {}
     for pos in response['def']:
-        key, values = pos['pos'], []
-        values.append(pos['tr'][0]['text'])
-        if 'syn' in pos['tr'][0]:
-            for syn in pos['tr'][0]['syn']:
-                values.append(syn['text'])
-        if values: res_dct[key] = values
+        if 'pos' in pos:
+            key, values = pos['pos'], []
+            values.append(pos['tr'][0]['text'])
+            if 'syn' in pos['tr'][0]:
+                for syn in pos['tr'][0]['syn']:
+                    values.append(syn['text'])
+            if values: res_dct[key] = values
     return res_dct
 
 
